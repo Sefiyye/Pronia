11d9926a6fbd3b34c85189542e394ba158319463
Sefiyye <tu7l1lxat@code.edu.az>
2022-05-24 09:35:19 +0400

    task

---
+++ WebApplication2/Models/Corusel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+    public class Corusel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Image { get; set; }
+        public string Fullname { get; set; }
+        public string About { get; set; }
+        public byte Number { get; set; }
+    }
+}
